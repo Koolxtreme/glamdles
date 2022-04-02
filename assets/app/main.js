1cffe13ce1879ff100f5d6a70a74c1abf8fd0fe1
Koolxtreme <koolxtreme@gmail.com>
2022-04-02 09:41:06 -0500

    first commit

---
+++ assets/app/main.js
@@ -0,0 +1,6 @@
+let navigationBtn = document.getElementById("navigation-button");
+let navSlide = document.getElementById("main-nav");
+
+navigationBtn.addEventListener('click', ()=>{
+    navSlide.classList.toggle("show")
+})
